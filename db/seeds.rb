70051237e9e685b08f3f7ac9b753100fd1853122
TakuyaKodama <t.kodama61@gmail.com>
2018-07-22 22:45:32 +0900

    Adds seeds script to create workers and shops

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |i|
+  worker = Worker.create(email: "sample#{i}@ex.com", password: "password")
+  shop = Shop.create(name: "ダミー店#{i}")
+  WorkerShop.create(worker: worker, shop: shop)
+end
