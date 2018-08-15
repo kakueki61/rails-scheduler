cfa19fd2284b69c3e1fec719b755893bc07bc3b5
TakuyaKodama <t.kodama61@gmail.com>
2018-08-15 23:00:52 +0900

    schema information

--- spec/factories/schedules.rb
+++ spec/factories/schedules.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: schedules
+#
+#  id         :bigint(8)        not null, primary key
+#  end_at     :datetime
+#  start_at   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  worker_id  :bigint(8)
+#
+# Indexes
+#
+#  index_schedules_on_worker_id  (worker_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (worker_id => workers.id)
+#
+
 FactoryGirl.define do
   factory :schedule do
     association :worker, strategy: :create
--- spec/factories/shops.rb
+++ spec/factories/shops.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: shops
+#
+#  id         :bigint(8)        not null, primary key
+#  name       :text(65535)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :shop do
     name { Faker::FunnyName.name }
